10397ca3981c941bb9493937bb7d81b5e1f2e25b
unknown <malak012426@gmail.com>
2023-08-01 15:20:27 +0100

    updated DOM

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // When the header_img is clicked, reload the page :
-document.getElementById('header_img').addEventListener('click', function() {
+document.getElementById("header_img").addEventListener("click", function () {
   location.reload();
 });
 
@@ -35,28 +35,82 @@ const itemList = [
 // Creating an object to store the quantities of each item
 let quantities = {};
 // Initialize the quantities for each item to be 0
-itemList.forEach(item => quantities[item.title] = 0);
+itemList.forEach((item) => (quantities[item.title] = 0));
 
 const rowElement = document.createElement("div");
 rowElement.className = "row justify-content-center";
 
 itemList.forEach((item) => {
   const cardElement = document.createElement("div");
-  cardElement.id = `${item.title.replace(/\s/g, '')}-card`; // replace spaces with nothing
-  cardElement.className = "card col-3 m-4 ";
-  cardElement.innerHTML = `
-    <img src="${item.imgSrc}" class="card-img-top" alt="${item.title}">
-    <div class="card-body">
-        <h5 class="card-title">${item.title}</h5>
-        <p class="card-text">Price: ${item.price}</p>
-        <span id="${item.title.replace(/\s/g, '')}-quantity">Quantity: 0</span>
-        <button onclick="increaseQuantity('${item.title}')" class="btn btn-primary m-1">+</button>
-        <button onclick="decreaseQuantity('${item.title}')" class="btn btn-primary m-1">-</button>
-        <button onclick="removeFromCart('${item.title.replace(/\s/g, '')}')" class="btn btn-danger m-1">Remove</button>
-        <button id="${item.title.replace(/\s/g, '')}-like" onclick="toggleLike('${item.title.replace(/\s/g, '')}')" class="btn btn-outline-primary m-1"><i class="fas fa-heart"></i></button>
-    </div>
-  `;
+  cardElement.id = `${item.title.replace(/\s/g, "")}-card`; // replace spaces with nothing
+  cardElement.className = "card col-2 m-3 ";
 
+  // Create the image object :
+  let MyImage = document.createElement("img");
+  MyImage.setAttribute("src", `${item.imgSrc}`);
+  MyImage.setAttribute("class", "card-img-top");
+  MyImage.setAttribute("alt", `${item.title}`);
+  cardElement.appendChild(MyImage);
+
+  // Create the body object :
+  const cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+  // Create the title object :
+  let myTitle = document.createElement("h5");
+  myTitle.setAttribute("class", "card-title");
+  myTitle.textContent = `${item.title}`;
+  cardBody.appendChild(myTitle);
+  // Create the price object :
+  let myPara = document.createElement("p");
+  myPara.setAttribute("class", "card-text");
+  myPara.textContent = `Price: ${item.price}`;
+  cardBody.appendChild(myPara);
+  //Create the quantity object :
+  let myQuantity = document.createElement("span");
+  myQuantity.id = `${item.title.replace(/\s/g, "")}-quantity`; // globally replace all spaces
+  myQuantity.textContent = "Quantity: 0";
+  cardBody.appendChild(myQuantity);
+  // Create the "+"button object :
+  let increase = document.createElement("button");
+  increase.className = "btn btn-primary m-1";
+  increase.textContent = "+";
+  increase.onclick = (function (itemTitle) {
+    return function () {
+      increaseQuantity(itemTitle);
+    };
+  })(item.title);
+  cardBody.appendChild(increase);
+  // Create the "-"button object :
+  let decrease = document.createElement("button");
+  decrease.className = "btn btn-primary m-1";
+  decrease.textContent = "-";
+  decrease.onclick = (function (itemTitle) {
+    return function () {
+      decreaseQuantity(itemTitle);
+    };
+  })(item.title);
+  cardBody.appendChild(decrease);
+  // Create the remove button object :
+  let removeButton = document.createElement("button");
+  removeButton.className = "btn btn-danger m-1";
+  removeButton.textContent = "Remove";
+  removeButton.onclick = function () {
+    removeFromCart(item.title.replace(/\s/g, ""));
+  };
+  cardBody.appendChild(removeButton);
+  // Create the liking button object :
+  let Mylike = document.createElement("button");
+  Mylike.id = `${item.title.replace(/\s/g, "")}-like`;
+  Mylike.className = "btn btn-outline-primary m-1";
+  Mylike.onclick = function () {
+    like(item.title.replace(/\s/g, ""));
+  };
+  let heart = document.createElement("i");
+  heart.className = "fas fa-heart";
+  Mylike.appendChild(heart);
+  cardBody.appendChild(Mylike);
+
+  cardElement.appendChild(cardBody);
   rowElement.appendChild(cardElement);
 });
 
@@ -64,27 +118,33 @@ document.getElementById("cart").appendChild(rowElement);
 
 function increaseQuantity(itemTitle) {
   quantities[itemTitle]++;
-  document.getElementById(`${itemTitle.replace(/\s/g, '')}-quantity`).innerText = `Quantity: ${quantities[itemTitle]}`;
+  document.getElementById(
+    `${itemTitle.replace(/\s/g, "")}-quantity`
+  ).innerText = `Quantity: ${quantities[itemTitle]}`;
 }
 
 function decreaseQuantity(itemTitle) {
   if (quantities[itemTitle] > 0) {
     quantities[itemTitle]--;
-    document.getElementById(`${itemTitle.replace(/\s/g, '')}-quantity`).innerText = `Quantity: ${quantities[itemTitle]}`;
+    document.getElementById(
+      `${itemTitle.replace(/\s/g, "")}-quantity`
+    ).innerText = `Quantity: ${quantities[itemTitle]}`;
   }
 }
 
 function removeFromCart(itemTitle) {
-  // Delete the item from the quantities object
-  delete quantities[itemTitle];
-  
-  // Remove the item's card from the DOM
-  const card = document.getElementById(`${itemTitle}-card`);
+  delete quantities[itemTitle]; // Delete the item from the quantities object
+  const card = document.getElementById(`${itemTitle}-card`); // Remove the item's card from the DOM
   card.remove();
 }
 
-function toggleLike(itemTitle) {
+function like(itemTitle) {
+  // We first get the like button for the item specified by itemTitle:
   const likeButton = document.getElementById(`${itemTitle}-like`);
-  likeButton.classList.toggle('btn-primary');
-  likeButton.classList.toggle('btn-outline-primary');
+  
+  // The classList property returns the class name(s) of an element as a DOMTokenList object. 
+  //The toggle() method of this object is used to add a class if it does not exist, and remove a class if it does exist. 
+  likeButton.classList.toggle("btn-primary"); 
+  likeButton.classList.toggle("btn-outline-primary");
+  //The net effect of these two lines is to switch between the 'btn-primary' and 'btn-outline-primary' styles whenever the like button is clicked.
 }
